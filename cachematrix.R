a3dfdd00c91468e0becd753c71e9629001484ba3
neophyte19 <neophyte19@users.noreply.github.com>
2014-06-18 16:06:45 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,36 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+ # takes no.of rows and no.of columns as argument and creates special "matrix" which can set / get matrix , set/get inv from cache
+makeCacheMatrix <- function(nrows,ncols,...){
+    inv<<- NULL    
+    set <- function(nrows, ncols,...)
+    {
+      if(nrows!=ncols)  #checks for squarematrix
+      {
+        message("not invertible")
+        return
+      }
+      mat <<- matrix(sample.int((nrows*ncols+100),nrows*ncols),nrows,ncols) #generate random numbers and create matrix
+      inv <<- NULL
+    }
+    get <- function() mat  #this funciton returns matrix
+    setinv <- function(inv) inv<<- inv #this function caches inverse
+    getinv <- function() inv  # this function returns inverse
+    list(set=set, get=get, setinv = setinv, getinv = getinv) 
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#this funciton returns inverse from cache / calculates inverse if already cached / matrix obj changed
+cacheSolve <- function(matobj, ...)
+{
+  inv <- matobj$getinv()
+  if(!is.null(inv))
+  {
+    message("getting cached data") #if the inverse is already calculated then returns data from cache
+    return(inv)
+  }
+  data <- matobj$get() #calculate inverse if not already in cache/matrix obj changed
+  inv <- solve(data)
+  matobj$setinv(inv) 
+  inv
 }
